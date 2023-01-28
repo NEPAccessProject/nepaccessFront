909a418095aa738bbc4b269b1d129a1484438665
jvinal <deathpizza@hotmail.com>
2023-01-27 17:22:02 -0700

    Hardcode bigorn backend URL

--- src/globals.js
+++ src/globals.js
@@ -35,7 +35,7 @@ const draftTypeLabelsLower = ["draft",
     "third draft supplemental"];
 
 const Globals = {
-    currentHost: new URL('https://mis-jvinalappl1.microagelab.arizona.edu:8080/'),
+    currentHost: new URL('https://bighorn.sbs.arizona.edu:8443/'),
 
     listeners: {},
 
@@ -68,11 +68,11 @@ const Globals = {
     
     // Set up globals like axios default headers and base URL
     setUp() {
-        if(window.location.hostname === 'mis-jvinalappl1.microagelab.arizona.edu' || window.location.hostname === 'www.nepaccess.org') {
-            this.currentHost = new URL(window.location.protocol + 'mis-jvinalappl1.microagelab.arizona.edu:8080/');
-        } else {
-            this.currentHost = new URL(window.location.protocol + window.location.hostname + ':8080/nepaBackend/');
-        } 
+        // if(window.location.hostname === 'mis-jvinalappl1.microagelab.arizona.edu' || window.location.hostname === 'www.nepaccess.org') {
+        //     this.currentHost = new URL(window.location.protocol + 'mis-jvinalappl1.microagelab.arizona.edu:8080/');
+        // } else {
+        //     this.currentHost = new URL(window.location.protocol + window.location.hostname + ':8080/nepaBackend/');
+        // } 
         // else if(window.location.hostname) {
         //     this.currentHost = new URL('https://' + window.location.hostname + ':8080/');
         // }
