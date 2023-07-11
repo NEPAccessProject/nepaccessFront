48332581a5836088011caffdea690ea7a6fcad5d
GeorgeMSchaffer <gschaffer@gmail.com>
2023-07-10 19:14:43 -0700

    Added document type checkboxes and counts

--- src/search/MainSearch/SearchSideBarFilters.jsx
+++ src/search/MainSearch/SearchSideBarFilters.jsx
@@ -81,24 +81,29 @@ export default function SearchSideBarFilters(props) {
     onTitleOnlyChecked,
     onProximityChange,
     onCooperatingAgencyChange,
+    onTypeChecked,
   } = useContext(SearchContext);
-  const { agencyRaw,state, county,stateOptions,countyOptions ,proximityDisabled, markup, cooperatingAgencyRaw } = searchState;
+  const { agencyRaw, state, county, stateOptions, countyOptions,
+    proximityDisabled, markup, cooperatingAgencyRaw, firstYear, lastYear, EISCount,
+    typeEA, typeDraft, typeFinal, typeNOI, draftCount, finalCount, noiCount, rodCount, scopingCount, eaCount
+
+  } = searchState;
   // Tried quite a bit but I can't force the calendar to Dec 31 of a year as it's typed in without editing the library code itself.
   // I can change the value but the popper state won't update to reflect it (even when I force it to update).
 
   // #region Date Handlers
-  const onEndDateChange = (date,evt) => {
-    console.log('onEndDateChange',date);
+  const onEndDateChange = (date, evt) => {
+    console.log('onEndDateChange', date);
     setSearchState({ ...searchState, endPublish: date.toLocaleString() });
     // }
   };
-  const onStartDateChange = (date,evt) => {
+  const onStartDateChange = (date, evt) => {
     console.log('onStartDateChange date', date);
     setSearchState({ ...searchState, startPublish: date.toLocaleString });
   };
 
-// #endregion  
-// region Render Return
+  // #endregion  
+  // region Render Return
   const classes = useStyles(theme);
   return (
     <>
@@ -120,57 +125,57 @@ export default function SearchSideBarFilters(props) {
             label="Search Only Within Titles"
           />
         </Item>
-{/* #region Proximity Filter */}
+        {/* #region Proximity Filter */}
         <Item>
-        <FormControl
-          fullWidth
-          xs={{
-            p: 1,
-            border: 0,
-            borderColor: 'grey.500',
-            borderRadius: 1,
-            mb: 1,
-            mt: 1,
-          }}
-        >
-          <Typography pb={1} variant="filterLabel">
-          Distance Between Search Terms:
-          </Typography>
-          <Autocomplete
-            id="proximity-select"
+          <FormControl
             fullWidth
-            autoComplete={true}
-            // autoHighlight={true}
-            tabIndex={11}
-            className={classes.autocomplete}
-            options={proximityOptions}
-            disablePortal={true}
-            // value={searchState.agencyRaw}
-            variant="standard"
-            // menuIsOpen={true}
-            onChange={onProximityChange}
-            getOptionLabel={(proximityOptions) => proximityOptions.label}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                value={searchState.proximity}
-                variant="outlined"
-                sx={{
-                  width: '100%',
-                  p: 0,
-                }}
-                placeholder="Type or Select a Word Distance"
-              />
-            )}
-          />
-        </FormControl>
+            xs={{
+              p: 1,
+              border: 0,
+              borderColor: 'grey.500',
+              borderRadius: 1,
+              mb: 1,
+              mt: 1,
+            }}
+          >
+            <Typography pb={1} variant="filterLabel">
+              Distance Between Search Terms:
+            </Typography>
+            <Autocomplete
+              id="proximity-select"
+              fullWidth
+              autoComplete={true}
+              // autoHighlight={true}
+              tabIndex={11}
+              className={classes.autocomplete}
+              options={proximityOptions}
+              disablePortal={true}
+              // value={searchState.agencyRaw}
+              variant="standard"
+              // menuIsOpen={true}
+              onChange={onProximityChange}
+              getOptionLabel={(proximityOptions) => proximityOptions.label}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  value={searchState.proximity}
+                  variant="outlined"
+                  sx={{
+                    width: '100%',
+                    p: 0,
+                  }}
+                  placeholder="Type or Select a Word Distance"
+                />
+              )}
+            />
+          </FormControl>
 
         </Item>
-{/* #endregion */}
+        {/* #endregion */}
       </Box>
 
       <Item>
-{/* #region Lead Agencies Filter */}
+        {/* #region Lead Agencies Filter */}
         <FormControl
           fullWidth
           xs={{
@@ -213,7 +218,7 @@ export default function SearchSideBarFilters(props) {
             )}
           />
         </FormControl>
-{/* #endregion */}
+        {/* #endregion */}
       </Item>
       <Item>
         <FormControl
@@ -261,119 +266,185 @@ export default function SearchSideBarFilters(props) {
       </Item>
       <Divider />
       <Item>
-      <Typography pb={1} variant="filterLabel">
-                State(s) or location(s):
-          </Typography>
-          <Autocomplete
-            id="state-select"
-            fullWidth
-            autoComplete={true}
-            // autoHighlight={true}
-            tabIndex={11}
-            className={classes.autocomplete}
-            options={searchState.stateOptions}
-            disablePortal={true}
-            // value={searchState.agencyRaw}
-            variant="standard"
-            // menuIsOpen={true}
-            onChange={onLocationChange}
-            getOptionLabel={(stateOptions) => `${stateOptions.label}`}
-            renderInput={(params) => (
-              <TextField
-                {...params}
-          
-                variant="outlined"
-                sx={{
-                  width: '100%',
-                  p: 0,
-                }}
-                placeholder={`Type or Select a State`} 
-              />
-            )}
-          />
+        <Typography pb={1} variant="filterLabel">
+          State(s) or location(s):
+        </Typography>
+        <Autocomplete
+          id="state-select"
+          fullWidth
+          autoComplete={true}
+          // autoHighlight={true}
+          tabIndex={11}
+          className={classes.autocomplete}
+          options={searchState.stateOptions}
+          disablePortal={true}
+          // value={searchState.agencyRaw}
+          variant="standard"
+          // menuIsOpen={true}
+          onChange={onLocationChange}
+          getOptionLabel={(stateOptions) => `${stateOptions.label}`}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+
+              variant="outlined"
+              sx={{
+                width: '100%',
+                p: 0,
+              }}
+              placeholder={`Type or Select a State`}
+            />
+          )}
+        />
       </Item>
       <Item>
-      <Typography pb={1} variant="filterLabel">
-            County/counties
-          </Typography>
-          <Autocomplete
-            id="county-select"
-            fullWidth
-            autoComplete={true}
-            // autoHighlight={true}
-            tabIndex={11}
-            className={classes.autocomplete}
-            options={countyOptions}
-            disablePortal={true}
-            // value={searchState.agencyRaw}
-            variant="standard"
-            // menuIsOpen={true}
-            onChange={onCountyChange}
-            getOptionLabel={(countyOptions) => `${countyOptions.label}`}
+        <Typography pb={1} variant="filterLabel">
+          County/counties
+        </Typography>
+        <Autocomplete
+          id="county-select"
+          fullWidth
+          autoComplete={true}
+          // autoHighlight={true}
+          tabIndex={11}
+          className={classes.autocomplete}
+          options={countyOptions}
+          disablePortal={true}
+          // value={searchState.agencyRaw}
+          variant="standard"
+          // menuIsOpen={true}
+          onChange={onCountyChange}
+          getOptionLabel={(countyOptions) => `${countyOptions.label}`}
 
-            renderInput={(params) => (
-              <TextField
-                {...params}
-                // value= {countyOptions.filter = (countyObj) => counties.includes(countyObj.value)}          
-                variant="outlined"
-                sx={{
-                  width: '100%',
-                  p: 0,
-                }}
-                placeholder={`Type or Select a County`} 
-              />
-            )}
-          />
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              // value= {countyOptions.filter = (countyObj) => counties.includes(countyObj.value)}          
+              variant="outlined"
+              sx={{
+                width: '100%',
+                p: 0,
+              }}
+              placeholder={`Type or Select a County`}
+            />
+          )}
+        />
       </Item>
       <Divider />
-        <Item>
-          <Typography pb={1} variant="filterLabel">
-            Date Range:
-          </Typography>
-          <Box
-            display={'flex'}
-            xs={12}
-            flexDirection={'column'}
-            border={0}
-            padding={0}
-            margin={0}
-            width={'100%'}
-            alignContent={'center'}
-            justifyItems={'center'}
-          >
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <Box item marginBottom={2} components={['DatePicker']} padding={0} width="100%">
-                <DatePicker
-                  name="startDate"
-                  onChange={(newValue,evt)=>onStartDateChange(newValue,evt)}
-                  id="date-picker-from"
-                  label="From:"
-                  value={searchState.startDate}
-                  sx={{
-                    width: '100%',
-                  }}
-                />
-              </Box>
-            </LocalizationProvider>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <Box components={['DatePicker']} padding={0} width="100%">
-                <DatePicker
-                  name="endDate"
-                  value={searchState.endDate}
-                  onChange={(newValue,evt)=> onEndDateChange(newValue,evt)}
-                  id="date-picker-to"
-                  label="To:"
-                  
-                  sx={{
-                    width: '100%',
+      <Item>
+        <Typography pb={1} variant="filterLabel">
+          Date Range:
+        </Typography>
+        <Box
+          display={'flex'}
+          xs={12}
+          flexDirection={'column'}
+          border={0}
+          padding={0}
+          margin={0}
+          width={'100%'}
+          alignContent={'center'}
+          justifyItems={'center'}
+        >
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Box item marginBottom={2} components={['DatePicker']} padding={0} width="100%">
+              <DatePicker
+                name="startDate"
+                onChange={(newValue, evt) => onStartDateChange(newValue, evt)}
+                id="date-picker-from"
+                label="From:"
+                value={searchState.startDate}
+                sx={{
+                  width: '100%',
+                }}
+              />
+            </Box>
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Box components={['DatePicker']} padding={0} width="100%">
+              <DatePicker
+                name="endDate"
+                value={searchState.endDate}
+                onChange={(newValue, evt) => onEndDateChange(newValue, evt)}
+                id="date-picker-to"
+                label="To:"
+
+                sx={{
+                  width: '100%',
 
 
-                  }}
-                />
-              </Box>
-            </LocalizationProvider>
-          </Box>
-        </Item>
+                }}
+              />
+            </Box>
+          </LocalizationProvider>
+        </Box>
+      </Item>
+      <Item>
+        <label className="sidebar-label-date">Document Type</label>
+        <div className="sidebar-checkboxes">
+          <div className="checkbox-container">
+            <label className="clickable checkbox-text">
+              <Checkbox
+                type="checkbox"
+                name="typeDraft"
+                className="sidebar-checkbox"
+                tabIndex="11"
+                checked={typeDraft}
+                onChange={onTypeChecked}
+              />
+              <span className="checkbox-text">
+                Draft EIS <i>{draftCount}</i>
+              </span>
+            </label>
+          </div>
+          <div className="checkbox-container">
+            <label className="clickable checkbox-text">
+              <Checkbox
+                type="checkbox"
+                name="typeFinal"
+                className="sidebar-checkbox"
+                tabIndex="12"
+                checked={typeFinal}
+                onChange={onTypeChecked}
+              />
+              <span className="checkbox-text">
+                Final EIS <i>{finalCount}</i>
+              </span>
+            </label>
+          </div>
+          <div className="checkbox-container">
+            <label className="clickable checkbox-text">
+              <Checkbox
+                type="checkbox"
+                name="typeEA"
+                className="sidebar-checkbox"
+                tabIndex="13"
+                checked={typeEA}
+                onChange={onTypeChecked}
+              />
+              <span className="checkbox-text">
+                EA <i>{eaCount}</i>
+              </span>
+            </label>
+          </div>
+          <div className="checkbox-container">
+            <label className="clickable checkbox-text">
+              <Checkbox
+                name="typeNOI"
+                id="typeNOI"
+                className="sidebar-checkbox"
+                tabIndex="14"
+                checked={typeNOI}
+                onChange={onTypeChecked}
+              />
+              <span className="checkbox-text">
+                NOI <i>{noiCount}</i>
+              </span>
+            </label>
+          </div>
+        </div>
+
+      </Item>
 
     </>
     //endregion
