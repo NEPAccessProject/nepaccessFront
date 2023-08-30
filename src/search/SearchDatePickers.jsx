f3f277f1ec13aa0fe76e635e0c69e907c47b61f2
GeorgeMSchaffer <gschaffer@gmail.com>
2023-08-30 15:06:17 -0700

    Made 100% width and removed padding overlap between the two dates

--- src/search/SearchDatePickers.jsx
+++ src/search/SearchDatePickers.jsx
@@ -1,10 +1,10 @@
 import React,{useContext} from 'react';
-import { Box } from '@mui/material';
+import { Paper,Box } from '@mui/material';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import SearchContext from './SearchContext';
-
+import Grid from '@mui/material/Unstable_Grid2';
 const onKeyDown = (e) => {
   if (e.key === "Tab") {
     if (this.datePickerStart) {
@@ -27,14 +27,15 @@ export default function SearchDatePickers(props){
     const {startDate, endDate,onEndDateChange,onStartDateChange} = props;
     const {state, setState} = useContext(SearchContext);
     return(
-        <>
-        
+        <Grid container id="date-filters-container">
             <LocalizationProvider dateAdapter={AdapterDayjs}>
             <Box item marginBottom={2} components={['DatePicker']}
              padding={0} 
              width="100%">
               <DatePicker
 //               ref={(ref) => (state.datePickerStart = ref)}
+                  name= "startPublish"
+                  id= "startPublish"
                   selected={state.startPublish}
                   onChange={onStartDateChange}
                   onKeyDown={onKeyDown}
@@ -47,6 +48,10 @@ export default function SearchDatePickers(props){
                   tabIndex="9"
                   popperPlacement="right"
                   isClearable
+                  sx={{
+                    height:50,
+                    width: '100%'
+                  }}
               />
             </Box>
           </LocalizationProvider>
@@ -54,6 +59,8 @@ export default function SearchDatePickers(props){
             <Box components={['DatePicker']} padding={0} width="100%">
               <DatePicker
                 //ref={(ref) => (this.datePickerEnd = ref)}
+              name="endPublish"
+                  id="endPublish"
                   selected={state.endPublish}
                   onChange={onEndDateChange}
                   onKeyDown={onKeyDown}
@@ -66,9 +73,13 @@ export default function SearchDatePickers(props){
                   tabIndex="10"
                   popperPlacement="right"
                   isClearable
+                   sx={{
+                    height:50,
+                    width: '100%'
+                  }}
               />
             </Box>
           </LocalizationProvider>
-        </>
+        </Grid>
     )
 }
