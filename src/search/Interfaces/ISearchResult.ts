3f788484f43670950d42d7713fb71e8f10f8e7b8
GeorgeMSchaffer <gschaffer@gmail.com>
2023-08-31 13:44:17 -0700

    Added interface for Search Result(s) and their records

---
+++ src/search/Interfaces/ISearchResult.ts
@@ -0,0 +1,46 @@
+
+export interface IRecord {
+  title: string;
+  agency: string;
+  cooperatingAgency: string | null;
+  commentDate: string;
+  registerDate: string;
+  state: string;
+  documentType: string;
+  filename: string;
+  commentsFilename: string;
+  size: number;
+  id: number;
+  luceneIds: string[];
+  folder: string;
+  plaintext: string[];
+  name: string;
+  link: string | null;
+  firstRodDate: string | null;
+  processId: number;
+  notes: string | null;
+  status: string | null;
+  subtype: string | null;
+  county: string;
+  action: string;
+  decision: string;
+  relevance: number;
+}
+
+export interface ISearchResult {
+  records: [IRecord];
+  processId: number;
+  isProcess?: boolean;
+  originalIndex: number;
+  registerDate?: any;
+  draftNoa?: string;
+  finalNoa?: string;
+  commentDate?: any;
+  title: string;
+  county?: string;
+  action?: string;
+  decision?: string;
+  relevance?: number;
+  agency?: string;
+  state?: string;
+}
--- src/search/Interfaces/index.ts
+++ src/search/Interfaces/index.ts
@@ -1,11 +1,13 @@
 import {IFiles,IEISDoc,IFile} from './IDocuments';
 import ISearchState from './ISearchState'
 import IAppState from './IAppState'
-
+import {ISearchResult, IRecord} from './ISearchResult'
 export {
   IFile,
   IEISDoc,
   IFiles,
   ISearchState,
   IAppState,
+  ISearchResult,
+  IRecord,
 }
