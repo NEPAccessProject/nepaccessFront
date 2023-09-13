0153e36b532324c1de9c03b37925fcd03cac2f2b
GeorgeMSchaffer <gschaffer@gmail.com>
2023-09-13 08:57:05 -0700

    Renamed file to patch the general pattern

--- src/search/PDFViewer/PDFViewerContainer.tsx
+++ src/search/PDFViewer/PDFViewerContainer.tsx
@@ -4,23 +4,22 @@ import {
   Typography
 } from '@mui/material';
 import React from 'react';
-import { IFile } from '../Interfaces';
+import { IEISDoc, IFile } from '../Interfaces';
 import PDFViewer from './PDFViewer';
 
 interface IProps {
     file: IFile
     currentFile: IFile;
+    doc: IEISDoc;
 }
 
-export default function PDFContainer(props:IProps) {
-//  console.log(`🚀 ~ file: PDFViewerDialog.jsx:298 ~ PDFContainer ~ props:`, props);
-  // const eisDoc: IEISDoc = file.eisdoc;
+export default function PDFViewerContainer(props:IProps) {
+  console.log(`🚀 ~ file: PDFViewerDialog.jsx:298 ~ PDFContainer ~ props:`, props);
   // const classes = useStyles();
-  const {file} = props;
-  const {eisdoc} = file;
+  const {file,doc} = props;
+  console.log("🚀 ~ file: PDFViewerContainer.tsx:19 ~ PDFViewerContainer ~ file:", file)
   console.log("🚀 ~ file: PDFContainer.tsx:35 ~ PDFContainer ~ props:", props)
-  if(!file) return (<div>File not found</div>);
-  const fileUrl = `/docs/${file.filename}`;
+  const fileUrl = `/docs/${file.folder}/${file.filename}`;
   console.log(`🚀 ~ file: PDFContainer.tsx:36 ~ PDFContainer ~ fileUrl:`, fileUrl);
   return (
 		<Paper
@@ -36,19 +35,22 @@ export default function PDFContainer(props:IProps) {
 					xs={12}
 					id='pdf-viewer-container-grid-item'
           >
-            <Typography>File ID: {file.id}</Typography>
+            {/* <Typography>File ID: {file.id}</Typography>
             <Typography>Process ID: {file.eisdoc.processId}</Typography>
             <Typography>Folder: {file.folder}</Typography>
+            <Typography>Relative Path {file.relativePath}</Typography>
             <Typography>Filename: {file.folder}</Typography>
-            <Typography>File Path: {fileUrl}</Typography>
-            <Typography>Title: {(eisdoc.title) ? eisdoc?.title : "N/A"}</Typography>
-            <Typography>Notes: {(eisdoc.notes) ? eisdoc?.notes : "N/A"}</Typography>
+            <Typography>File Path: {fileUrl}</Typography> */}
+            {/* <Typography>Title: {(eisdoc.title) ? eisdoc?.title : "N/A"}</Typography>
+            <Typography>Notes: {(eisdoc.notes) ? eisdoc?.notes : "N/A"}</Typography> */}
+          <Typography>Start PDF Viewer</Typography>
 					<PDFViewer
 						{...props}
             
             file={file}
-						fileUrl={'../../assets/example.pdf'}
+						fileUrl={fileUrl}
 					/> 
+                    <Typography>END PDF Viewer</Typography>
 				</Grid>
 			</Grid>
 		</Paper>
