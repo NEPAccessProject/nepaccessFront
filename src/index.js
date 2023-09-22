fc2f4bf2bfb15a9efe698b7a65acee7b5d262583
GeorgeMSchaffer <gschaffer@gmail.com>
2023-09-21 20:16:02 -0700

    add sentry error logger trial

--- src/index.js
+++ src/index.js
@@ -8,6 +8,22 @@ import Main from './Main.js';
 
 import * as serviceWorker from './serviceWorker';
 import { MuiThemeProvider } from '@material-ui/core';
+import * as Sentry from '@sentry/browser';
+Sentry.init({
+    dsn: "https://de8e659cc5086223380e30e385e62907@o4505920810778624.ingest.sentry.io/4505920813400064",
+    integrations: [
+      new Sentry.BrowserTracing({
+        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: ["localhost", "https://nepaccess.gov\/*"],}),
+      new Sentry.Replay(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+  
 
 ReactDOM.render(
     <BrowserRouter>
