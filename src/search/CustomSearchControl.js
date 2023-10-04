27968ab1725464e53061ab5e921a8f2043e74767
GeorgeMSchaffer <gschaffer@gmail.com>
2023-10-04 12:53:57 -0700

    remove logging

--- src/search/CustomSearchControl.js
+++ src/search/CustomSearchControl.js
@@ -3,10 +3,7 @@ import { useMap } from "react-leaflet";
 import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch";
 
 const CustomSearchControl = (props) => {
-    console.log(`🚀 ~ file: CustomSearchControl.js:6 ~ CustomSearchControl ~ props:`, props);
-
     const map = useMap();
-
     useEffect(() => {
         const searchControl = new GeoSearchControl({
             provider: new OpenStreetMapProvider(),
