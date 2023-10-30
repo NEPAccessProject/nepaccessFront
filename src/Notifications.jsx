ad325aebcd31ea480d10b917e76faa340d5bc791
GeorgeMSchaffer <gschaffer@gmail.com>
2023-10-30 10:12:47 -0700

    Updated how notification handle props.

--- src/Notifications.jsx
+++ src/Notifications.jsx
@@ -1,22 +1,22 @@
-import react, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Paper, Snackbar, Alert } from '@mui/material';
+import PropTypes  from 'prop-types';
 export default function Notifications(props) {
-  const {message,messageType} = props;
-  const [state, setState] = useState({
-    open: false,
-  })
-
+  console.info(`file: Notifications.jsx:5 ~ Notifications ~ props:`, props);
+  const {message="",messageType = "info",open=false} = props;
   return (
     <>
-      <Snackbar open={message} autoHideDuration={6000} onClose={handleClose}>
+    {open &&
+      <Snackbar anchorOrigin={{vertical: 'top', horizontal: 'right'}} transitionDuration={{enter: 100, exit: 500}} open={open} autoHideDuration={3000}>
         <Alert severity={messageType}>
           {message}
         </Alert>
       </Snackbar>
+      }
     </>
   )
 }
 Notifications.propTypes = {
   message: PropTypes.string.isRequired,
-  type: React.propTypes.oneOf(['error','success', 'warning', 'info']).isRequired,
+  type: PropTypes.oneOf(['error','success', 'warning', 'info']).isRequired,
 }
