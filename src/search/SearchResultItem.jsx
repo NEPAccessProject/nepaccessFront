d3a407012225c5be1319b0f53757913f13837f31
GeorgeMSchaffer <gschaffer@gmail.com>
2023-10-30 10:11:37 -0700

    Added PDF Viewer (WIP) back in

--- src/search/SearchResultItem.jsx
+++ src/search/SearchResultItem.jsx
@@ -1,4 +1,4 @@
-import { Alert,Button, Box, Divider, Grid, Paper,Snackbar ,Typography } from '@mui/material';
+import { Alert, Button, Box, Divider, Grid, Paper, Snackbar, Typography } from '@mui/material';
 //import Grid from '@mui/material/Unstable_Grid2';
 import { makeStyles } from '@mui/styles';
 import React, { useContext, useEffect, useRef, useState } from 'react';
@@ -9,16 +9,16 @@ import RenderSnippets from './SearchResultSnippets.jsx';
 import { styled } from '@mui/styles';
 import DownloadFiles from '../DownloadFiles';
 import DownloadFile from '../DownloadFile';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 const DataCell = styled(Grid)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   color: theme.palette.text.secondary,
   border: 1,
   item: true,
-  padding:2,
-  paddingTop:15,
-  paddingBottom:15,
+  padding: 2,
+  paddingTop: 15,
+  paddingBottom: 15,
   spacing: 2,
   borderColor: 'black',
   display: 'flex',
@@ -79,20 +79,6 @@ export default function SearchResultItem(props) {
   }, [])
 
 
-  function onPDFPreviewToggle(evt, fileId, record) {
-    console.log('PDF VIEW Toggle,evt', evt);
-    evt.preventDefault();
-    setIsPDFViewOpen(true)
-  }
-
-  function showPDFPreview(evt, fileId, record) {
-    console.log('PDF VIEW Toggle,evt', evt);
-    evt.preventDefault();
-    setIsPDFViewOpen(true)
-  }
-  function onDocumentLoadSuccess({ numPages }) {
-    setState({ ...state, numPages: numPages });
-  }
 
   //[TODO] Add download functionality
   const handleDownloadClick = (evt, id) => {
@@ -125,7 +111,7 @@ export default function SearchResultItem(props) {
 
       {records.map((record, idx) => (
         <div key={record.id} id={`render-record-root`} padding={3}>
-          <RenderRecord record={record} />
+          <RenderRecord record={record} isPDFViewOpen={isPDFViewOpen} />
         </div>
       ))}
 
@@ -135,6 +121,7 @@ export default function SearchResultItem(props) {
 
 const RenderRecord = (props) => {
   const { record } = props;
+  const [isPDFViewOpen, setIsPDFViewOpen] = useState(false);
   const {
     action,
     agency,
@@ -161,12 +148,56 @@ const RenderRecord = (props) => {
   //  const year = commentDate && commentDate.length > 0 ? new Date(commentDate).getFullYear() : 'N/A';
   const year = 2023;
   const classes = useStyles(theme);
+  const [modalOpen,setModalOpen] = useState(false);
+	const [currentModalId,setCurrentModalId] = useState(0)
+
+  function showPDFPreview(evt, fileId, record) {
+    event.preventDefault()
+    const { target: { dataset: { modal } } } = event
+    setCurrentModalId(`modal-${id}`)
+    setModalOpen(true)
+    if (modal) setModal(modal)
+  }
+  function onDocumentLoadSuccess({ numPages }) {
+    setState({ ...state, numPages: numPages });
+  }
+
+  //[TODO] Add download functionality
+  const handleDownloadClick = (evt, id) => {
+    evt.preventDefault();
+    console.log('Download ID Value and filename', id, filename);
+  };
+
+  const ModalManager = (props) => {
+    console.log(`file: SearchResultItem.jsx:195 ~ ModalManager ~ props:`, props);
+    const { record = {}, id = 0, modal = '', isOpen = false } = props
+    return (
+      <>
+        {isOpen &&
+          <PDFViewerDialog
+            id={`pdf-dialog-${id}`}
+            name={`modal-${record.id}`}
+            record={record}
+            //isOpen={isOpen[record.id]}
+            isOpen={isOpen && currentModalId === `modal-${record.id}`}
+            onDialogClose={(evt) => onDialogClose(evt, record.id)}
+          />
+        }
+      </>
+    )
+  };
+
+  const onDialogClose = (evt, id) => {
+    setCurrentModalId(null)
+    setModalOpen(false)
+    setModal('')
+  }
   return (
-<>
+    <>
       <Paper elevation={0} style={{
-          marginLeft:2,
-          marginRight:2,
-          marginTop:10,
+        marginLeft: 2,
+        marginRight: 2,
+        marginTop: 10,
       }}>
         <Grid container>
           <DataCell>
@@ -190,7 +221,7 @@ const RenderRecord = (props) => {
               <Typography id="snippets-title" variant='h5' >{(title) ? title : ''}</Typography>
             </DataCell>
             <Grid container xs={2} flex={1} id="button-grid-container">
-              <DataCell item id="preview-button-grid-item" item display={'flex'} xs={12} >
+              <DataCell item id="download-button-grid-item" item display={'flex'} xs={6} >
                 {/* <Button onClick={(evt) => handleDownloadClick(evt)} variant='contained' color="primary">
                   Download
                 </Button> */}
@@ -201,6 +232,20 @@ const RenderRecord = (props) => {
                   filename={record.filename}
                 />
               </DataCell>
+              <DataCell item id="preview-button-grid-item" item display={'flex'} xs={6}>
+                <ModalManager
+                  record={record}
+                  id={record.id}
+                  isOpen={isPDFViewOpen}
+                  closeFn={onDialogClose}
+                  modal={modalOpen} />
+                <Button
+                  onClick={(evt) => showPDFPreview(evt, record.id, record)}
+                  variant='contained'
+                  color="primary">
+                  Preview - {isPDFViewOpen ? 'open' : 'closed'}
+                </Button>
+              </DataCell>
             </Grid>
           </Grid>
           <Grid id="render-snippets-record-grid-container" container xs={12}>
@@ -210,6 +255,6 @@ const RenderRecord = (props) => {
         </Grid>
 
       </Paper>
-</>
+    </>
   )
 }
