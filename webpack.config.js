ad067083ebe8dd59d85c1e604aa18a777480f04f
GeorgeMSchaffer <gschaffer@gmail.com>
2023-11-09 15:37:39 -0700

    Added aditonal config settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-    'devtool': 'source map'
+    env: {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-hooks/recommended'
+    ],
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 12,
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+    ],
+    'rules': {
+
+    }
 }
