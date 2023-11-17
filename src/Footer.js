f80be0fb31602f021a2f90ea1cb8ed8b8e1e05ec
GeorgeMSchaffer <gschaffer@gmail.com>
2023-11-16 18:25:13 -0700

    Disabling for now

--- src/Footer.js
+++ src/Footer.js
@@ -1,22 +1,25 @@
 import React from 'react';
-
+import Grid from '@mui/material/Unstable_Grid2';
 export default class Footer extends React.Component {
 
     // constructor(props) {
     //     super(props);
     // }
-    
+
     render () {
         return (
-            <div id="footer">
+          <>
+            {/* [TODO] Need to figure out how to get this to work in the future
+            <Grid container xs={12} display={"flex"} justifyContent={"center"}>
                 <a href="https://nepaccess.org/privacy-policy" rel="noreferrer"
                         target="_blank">
                     Privacy policy
                 </a>
-            </div>
+            </Grid> */}
+            </>
         )
     }
-    
+
 	// componentDidMount() {
 	// }
 }
