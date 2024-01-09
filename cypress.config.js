6eca7e42358553722047146c4ff7d70aba20d635
GeorgeMSchaffer <gschaffer@gmail.com>
2024-01-09 16:04:30 -0700

    Pulled over Cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,37 +1,18 @@
 const { defineConfig } = require("cypress");
-
-// https://github.com/cypress-visual-regression/cypress-visual-regression
-const getCompareSnapshotsPlugin = require('cypress-visual-regression/dist/plugin');
-
-//const HtmlWebPackPlugin = require("html-webpack-plugin");
+//const getCompareSnapshotsPlugin = require('.js');
 
 module.exports = defineConfig({
-  projectId: "7x4r9s",
-  key: "a42c44b4-bbb3-40c5-9605-22fc2bedb05f",
-  screenshotOnRunFailure: true,
-  video: true,
-  chromeWebSecurity: false,
-  experimentalStudio: true,
-  screenshotOnRunFailure: true,
-  screenshotsFolder: "cypress/screenshots",
-  videosFolder: "cypress/videos",
-  env: {
-    "ALWAYS_GENERATE_DIFF": true,
-    "ALLOW_VISUAL_REGRESSION_TO_FAIL": true
-  },
+  screenshotsFolder: "./cypress/snapshots/actual",
+  trashAssetsBeforeRuns: true,
+  video: false,
+
   e2e: {
     setupNodeEvents(on, config) {
-      console.log("Cypress e2e config", config);
-      getCompareSnapshotsPlugin(on, config);
-
-      // implement node event listeners here
+      //    getCompareSnapshotsPlugin(on, config);
     },
   },
 
   component: {
-    webpackConfig: {
-      
-    },
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
